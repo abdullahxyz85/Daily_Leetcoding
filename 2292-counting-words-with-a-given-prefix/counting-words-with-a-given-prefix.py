b48be2727ad1b28c80e57ccfbe462b8c9889af70
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-01-09 09:26:45 +0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (20.46%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
@@ -1,7 +1,16 @@
 class Solution:
     def prefixCount(self, words: List[str], pref: str) -> int:
-        count = 0
-        for word in words:
-            if word.startswith(pref):
-                count +=1
-        return count
+            count = 0
+            pref_length = len(pref)
+            for word in words:
+                if word[:pref_length] == pref:
+                  count +=1
+            return count
+        
+        
+        
+        # count = 0
+        # for word in words:
+        #     if word.startswith(pref):
+        #         count +=1
+        # return count
