bc495915f5c021a30f5bfddee7daf2095d61bde4
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-01-13 11:27:08 +0500

    Time: 0 ms (100.00%) | Memory: 18 MB (9.79%) - LeetSync

--- 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.py
+++ 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.py
@@ -1,11 +1,8 @@
 class Solution:
     def sumOfSquares(self, nums: List[int]) -> int:
-        n = len(nums)  # Length of the array
-        special_sum = 0
-
-        # Iterate through all indices (1-indexed)
-        for i in range(1, n + 1):
-            if n % i == 0:  # Check if i divides n
-                special_sum += nums[i - 1] ** 2  # Add square of the special element
-
-        return special_sum
+       n = len(nums)
+       special_sum = 0
+       for i in range(1,n+1):
+            if n % i == 0:
+                special_sum += nums[i - 1] ** 2
+       return special_sum
