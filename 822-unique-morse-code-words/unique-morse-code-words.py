da0609aa4bc464a7195f6967260a7685e68594f8
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-01-16 10:47:54 +0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (20.17%) - LeetSync

--- 822-unique-morse-code-words/unique-morse-code-words.py
+++ 822-unique-morse-code-words/unique-morse-code-words.py
@@ -1,17 +1,25 @@
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
-        m = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
-        a = "abcdefghijklmnopqrstuvwxyz"
-        p = []
-        ans = []
-        for q in words:
-            b = ""
-            for i in q:
-                b += m[a.index(i)]
-            p.append(b)
-        for q in p:
-            if q not in ans:
-                ans.append(q)
-        return len(ans)
+        morse_code = [
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-",
+            ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-",
+            ".--", "-..-", "-.--", "--.."
+        ]
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        morse_words = []  # List to store Morse code translations of words
+        unique_morse = []  # List to store unique Morse code representations
+        
+        for word in words:
+            morse_representation = ""  # To build the Morse code for the current word
+            for char in word:
+                morse_representation += morse_code[alphabet.index(char)]  # Convert char to Morse
+            morse_words.append(morse_representation)  # Add Morse code of the word to the list
+        
+        for morse_word in morse_words:
+            if morse_word not in unique_morse:  # Check if it's a unique representation
+                unique_morse.append(morse_word)
+        
+        return len(unique_morse)  # Return the count of unique representations
+
 
 
