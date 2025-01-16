062e8282b62e45111240ca1e02588911c42b582e
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-01-16 10:42:24 +0500

    Time: 3 ms (24.65%) | Memory: 18 MB (9.27%) - LeetSync

--- 822-unique-morse-code-words/unique-morse-code-words.py
+++ 822-unique-morse-code-words/unique-morse-code-words.py
@@ -1,25 +1,17 @@
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
-        morse_code = [
-            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", 
-            ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", 
-            ".--", "-..-", "-.--", "--.."
-        ]
-        
-        transformations = []
-        
-        for word in words:
-            transformation = ""
-            for char in word:
-                index = 0
-                for letter in "abcdefghijklmnopqrstuvwxyz":
-                    if char == letter:
-                        break
-                    index += 1
-                transformation += morse_code[index]
-            if transformation not in transformations:
-                transformations.append(transformation)
-        
-        return len(transformations)
+        m = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        a = "abcdefghijklmnopqrstuvwxyz"
+        p = []
+        ans = []
+        for q in words:
+            b = ""
+            for i in q:
+                b += m[a.index(i)]
+            p.append(b)
+        for q in p:
+            if q not in ans:
+                ans.append(q)
+        return len(ans)
 
 
