75612f72700d87045eea716b845648bdae32d03d
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-02-03 11:28:36 +0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (40.46%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.py
+++ 953-reverse-only-letters/reverse-only-letters.py
@@ -2,17 +2,15 @@ class Solution:
     def reverseOnlyLetters(self, s: str) -> str:
         alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         res = []
-        s = list(s)  # Convert string to list for modification
-        i, j = 0, len(s) - 1
-
-        while i < j:
+        s = list(s)
+        i,j = 0, len(s) - 1
+        while i<j:
             if s[i] not in alphabets:
-                i += 1
+                i +=1
             elif s[j] not in alphabets:
                 j -= 1
             else:
                 s[i], s[j] = s[j], s[i]
                 i += 1
                 j -= 1
-        
         return "".join(s)
