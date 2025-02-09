7b23176619b270b1268a1244cf0e5f50f6bf57c0
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-02-09 11:07:08 +0500

    Time: 6 ms (31.45%) | Memory: 17.9 MB (31.83%) - LeetSync

--- 507-perfect-number/perfect-number.py
+++ 507-perfect-number/perfect-number.py
@@ -1,17 +1,17 @@
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
         if num <= 1:
-            return False  # 1 is not a perfect number
+            return False 
 
-        sum_divisors = 1  # Start with 1 because every number is divisible by 1
-        i = 2  # Start from 2 because 1 is already counted
+        sum_divisors = 1 
+        i = 2 
 
-        while i * i <= num:  # Only iterate up to sqrt(num)
-            if num % i == 0:  # If 'i' is a divisor
-                sum_divisors += i  # Add 'i'
-                if i != num // i:  # Avoid counting the square root twice
-                    sum_divisors += num // i  # Add its pair divisor
-            i += 1  # Move to the next divisor
+        while i * i <= num: 
+            if num % i == 0: 
+                sum_divisors += i 
+                if i != num // i:  
+                    sum_divisors += num // i 
+            i += 1 
 
-        return sum_divisors == num  # Check if sum equals num
+        return sum_divisors == num  
 
