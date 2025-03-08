a72a4cddf69c470bf932884fdfe38853cc5ff7a5
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-03-08 19:31:36 +0500

    Time: 3 ms (3.06%) | Memory: 17.6 MB (95.21%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.py
+++ 1448-maximum-69-number/maximum-69-number.py
@@ -1,8 +1,9 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        num_str = list(str(num))  # Convert number to list of characters for easy modification
+        num_str = list(str(num)) 
+
         for i in range(len(num_str)):
-            if num_str[i] == '6':  # Find the first '6'
-                num_str[i] = '9'  # Change it to '9'
-                break  # Stop after the first change
+            if num_str[i] == '6': 
+                num_str[i] = '9' 
+                break 
         return int("".join(num_str))  # Convert back to integer and return
