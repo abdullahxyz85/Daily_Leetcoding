4656b1b2df7778fae59ff517c22da4a3b56c77ba
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-03-20 13:06:47 +0500

    Time: 15 ms (9.91%) | Memory: 17.9 MB (53.05%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -3,7 +3,7 @@ from typing import List
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         for i in range(len(arr)):
-            for j in range(len(arr)):  # Fix: Start from 0
-                if i != j and (arr[i] == 2 * arr[j] or arr[j] == 2 * arr[i]):  # Fix: Correct condition
+            for j in range(len(arr)):  
+                if i != j and (arr[i] == 2 * arr[j] or arr[j] == 2 * arr[i]): 
                     return True
         return False
