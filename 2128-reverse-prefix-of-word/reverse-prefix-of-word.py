621dc5a107b6671c4a448600fe573c3ad10a4872
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-03-24 15:26:42 +0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (83.62%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        word_list = list(word)
+        
+        index = -1
+        for i in range(len(word_list)):
+            if word_list[i] == ch:
+                index = i
+                break
+        
+        if index == -1:
+            return word
+        
+        left, right = 0, index
+        while left < right:
+            word_list[left], word_list[right] = word_list[right], word_list[left]
+            left += 1
+            right -= 1
+        
+        return "".join(word_list)
