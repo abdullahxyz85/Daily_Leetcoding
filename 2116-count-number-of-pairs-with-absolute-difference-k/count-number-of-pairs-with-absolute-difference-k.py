242a0922672dbbdb772a4b7afbd84ac9c2e7accf
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-03-25 11:11:30 +0500

    Time: 75 ms (38.59%) | Memory: 17.9 MB (19.06%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.py
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.py
@@ -6,3 +6,16 @@ class Solution:
                 if abs(nums[i] - nums[j]) == k:
                     count += 1
         return count
+
+#  Second Approach:
+# class Solution:
+#     def countKDifference(self, nums: List[int], k: int) -> int:
+#         freq = defaultdict(int)  # Dictionary to store frequency of numbers
+#         count = 0
+        
+#         for num in nums:
+#             count += freq[num - k]  # Check if (num - k) exists in hashmap
+#             count += freq[num + k]  # Check if (num + k) exists in hashmap
+#             freq[num] += 1  # Store the count of the current number
+        
+#         return count
