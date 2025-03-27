6c6ede39bf28c6d2e7bf85a8c56d684520d7e41a
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-03-27 12:35:50 +0500

    Time: 3 ms (30.37%) | Memory: 17.9 MB (37.14%) - LeetSync

--- 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.py
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.py
@@ -11,3 +11,16 @@ class Solution:
         if single_sum>double_sum or double_sum > single_sum:
             return True
         return False
+
+    #     class Solution:
+    # def canAliceWin(self, nums: List[int]) -> bool:
+    #     single_sum = 0
+    #     double_sum = 0
+        
+    #     for num in nums:
+    #         if num < 10:  
+    #             single_sum += num
+    #         else:  
+    #             double_sum += num
+        
+    #     return single_sum > double_sum or double_sum > single_sum
