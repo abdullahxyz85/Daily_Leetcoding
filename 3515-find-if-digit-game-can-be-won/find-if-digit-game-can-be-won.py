c336fa2a6b2c734270616005afdd01559fa5ae39
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-03-27 12:34:22 +0500

    Time: 3 ms (30.37%) | Memory: 18 MB (17.60%) - LeetSync

--- 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.py
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.py
@@ -2,11 +2,12 @@ class Solution:
     def canAliceWin(self, nums: List[int]) -> bool:
         single_sum = 0
         double_sum = 0
-        
-        for num in nums:
-            if num < 10:  
-                single_sum += num
-            else:  
-                double_sum += num
-        
-        return single_sum > double_sum or double_sum > single_sum
+        for i in nums:
+            i = str(i)
+            if len(i) == 2:
+                double_sum += int(i)
+            else:
+                single_sum += int(i)
+        if single_sum>double_sum or double_sum > single_sum:
+            return True
+        return False
