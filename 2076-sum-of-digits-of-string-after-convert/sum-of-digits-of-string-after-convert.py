6bdca4a13c02f52fd837b6197f880bdd3af68a2a
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-03-28 19:21:27 +0500

    Time: 2 ms (73.50%) | Memory: 17.9 MB (36.11%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
@@ -0,0 +1,22 @@
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        alphabet_values = {
+            'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 
+            'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 
+            'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26
+        }
+
+        # Convert string to number using dictionary
+        num_str = ""
+        for ch in s:
+            num_str += str(alphabet_values[ch])
+
+        # Step 2: Sum the digits k times
+        while k > 0:
+            sum_digits = 0
+            for digit in num_str:
+                sum_digits += int(digit)
+            num_str = str(sum_digits)
+            k -= 1
+
+        return int(num_str)
