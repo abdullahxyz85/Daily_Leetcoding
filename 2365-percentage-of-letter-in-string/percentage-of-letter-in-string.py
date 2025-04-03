64ad7716d2f0c87c6d22bcb2f69a99090111b526
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-04-03 16:31:20 +0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (67.87%) - LeetSync

--- 2365-percentage-of-letter-in-string/percentage-of-letter-in-string.py
+++ 2365-percentage-of-letter-in-string/percentage-of-letter-in-string.py
@@ -1,15 +1,16 @@
 class Solution:
     def percentageLetter(self, s: str, letter: str) -> int:
             freq = {}
-            for i in s:
+            for i in s:                                                                       # s = "foobar", letter = "d"
                 if i in freq:
                     freq[i] += 1
                 else:
                     freq[i] = 1
             
-            letter_count = 0
             if letter in freq:
-                letter_count = freq[letter]
+                letter_count = freq[letter] # freq["o"]
+            else:
+                letter_count = 0
             
             percentage = (letter_count / len(s)) * 100
             
