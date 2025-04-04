a780b07ad28f5c0b07f8100862181fec41713add
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-04-04 11:05:50 +0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (5.96%) - LeetSync

--- 2480-find-subarrays-with-equal-sum/find-subarrays-with-equal-sum.py
+++ 2480-find-subarrays-with-equal-sum/find-subarrays-with-equal-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def findSubarrays(self, nums: List[int]) -> bool:
         seen_sums = set()  
-        for i in range(len(nums) - 1):
+        for i in range(len(nums) - 1):        
             current_sum = nums[i] + nums[i + 1]
 
             if current_sum in seen_sums:
@@ -10,3 +10,7 @@ class Solution:
             seen_sums.add(current_sum)
 
         return False
+
+    #[4, 2, 4]
+    #i = 1
+    # nums[1] + nums[1 + 1] 2 + 4 = 6
