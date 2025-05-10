7241a1509fc1608e4bf87f4ccd3bea8fa9855862
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-05-10 18:50:55 +0500

    Time: 0 ms (100%), Space: 18 MB (17.66%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,39 @@
+# class Solution:
+#     def isHappy(self, n: int) -> bool:
+#         if n < 10:
+#             return False
+#         while n > 10:
+#             sum_sqr_digits = 0
+#             while n > 0:
+#                 sum_sqr_digits += (n % 10) ** 2
+#                 n //= 10
+#             n = sum_sqr_digits
+#         if sum_sqr_digits == 1:
+#             return True
+        
+# class Solution:
+#     def isHappy(self, n: int) -> bool:
+#         seen = set()
+        
+#         while n != 1 and n not in seen:
+#             seen.add(n)
+#             sum_sqr_digits = 0
+#             while n > 0:
+#                 sum_sqr_digits += (n % 10) ** 2
+#                 n //= 10
+#             n = sum_sqr_digits
+        
+#         return n == 1
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            sum_sqr_digits = 0
+            while n > 0:
+                digit = n % 10
+                sum_sqr_digits += digit * digit
+                n //= 10
+            n = sum_sqr_digits
+        return n == 1
