d9eef5008434e933fa84b2e316832b4ced792c52
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-05-25 16:09:46 +0500

    Time: 8 ms (4.71%), Space: 17.8 MB (52.57%) - LeetHub

--- 2337-remove-digit-from-number-to-maximize-result/2337-remove-digit-from-number-to-maximize-result.py
+++ 2337-remove-digit-from-number-to-maximize-result/2337-remove-digit-from-number-to-maximize-result.py
@@ -4,7 +4,6 @@ class Solution:
 
         for i in range(len(number)):
             if number[i] == digit:
-                # Build new string manually without using slicing
                 new_number = ""
                 for j in range(len(number)):
                     if j != i:
