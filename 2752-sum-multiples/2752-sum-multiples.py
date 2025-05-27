80757941eadfac147d320571dbc5ebe2ea7ada87
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-05-27 18:44:54 +0500

    Time: 23 ms (82.3%), Space: 18 MB (23.6%) - LeetHub

--- 2752-sum-multiples/2752-sum-multiples.py
+++ 2752-sum-multiples/2752-sum-multiples.py
@@ -1,11 +1,11 @@
 class Solution:
     def sumOfMultiples(self, n: int) -> int:
         total = 0
-        for i in range(1, n + 1):  # Fix 1: loop from 1 to n
+        for i in range(1, n + 1):  
             if i % 3 == 0:
                 total += i
             elif i % 5 == 0:
                 total += i
-            elif i % 7 == 0:       # Fix 2: check for 7 instead of n
+            elif i % 7 == 0:     
                 total += i
         return total
