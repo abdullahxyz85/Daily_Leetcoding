b02dc0236c273fc5f7fae72486a3ebdadfe01857
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-06-18 09:30:44 +0500

    Time: 0 ms (100%), Space: 18.8 MB (20.77%) - LeetHub

--- 0944-smallest-range-i/0944-smallest-range-i.py
+++ 0944-smallest-range-i/0944-smallest-range-i.py
@@ -3,8 +3,6 @@ class Solution:
         max_val = max(nums)
         min_val = min(nums)
         
-        # Calculate the possible minimum score after applying the operation
         score = max_val - min_val - 2 * k
         
-        # If score is negative, it means we can make all elements equal or overlap
         return max(0, score)
