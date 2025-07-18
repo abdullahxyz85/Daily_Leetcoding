d7c61e56587d3bc2fd74f32135eb7b1ee49bcffd
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-07-18 16:01:36 +0500

    Time: 225 ms (92%), Space: 19.8 MB (35.6%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.py
@@ -1,13 +1,13 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
         result = []
-        count = 0  # to count consecutive same characters
+        count = 0  
 
         for char in s:
             if result and char == result[-1]:
                 count += 1
             else:
-                count = 1  # reset the count when char is different
+                count = 1 
 
             if count < 3:
                 result.append(char)
