997fb24c21bdaeab018f1433ba6ac16989961320
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-07-25 18:04:39 +0500

    Time: 2 ms (65.03%), Space: 17.8 MB (75.26%) - LeetHub

---
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        b_count = a_count = l_count = o_count = n_count = 0  
+
+        for char in text:
+            if char == 'b':
+                b_count += 1
+            elif char == 'a':
+                a_count += 1
+            elif char == 'l':
+                l_count += 1
+            elif char == 'o':
+                o_count += 1
+            elif char == 'n':
+                n_count += 1
+
+        l_count //= 2  
+        o_count //= 2  
+
+        return min(b_count, a_count, l_count, o_count, n_count)
+                
