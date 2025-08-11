f86c35e2c0202dce076624613082cb4aea72e9a0
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-08-11 14:53:35 +0500

    Time: 0 ms (100%), Space: 17.7 MB (66.41%) - LeetHub

--- 2507-number-of-common-factors/2507-number-of-common-factors.py
+++ 2507-number-of-common-factors/2507-number-of-common-factors.py
@@ -2,8 +2,7 @@ class Solution:
     def commonFactors(self, a: int, b: int) -> int:
         count = 0
         
-        # Check every number from 1 to the smaller of a and b
         for i in range(1, min(a, b) + 1):
-            if a % i == 0 and b % i == 0:  # if i divides both a and b
+            if a % i == 0 and b % i == 0:  
                 count += 1
         return count
