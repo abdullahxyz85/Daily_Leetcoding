3e9e12bea38b4b2198a74f6e5431548521fee3f0
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-09-24 12:58:18 +0500

    Time: 0 ms (100%), Space: 17.8 MB (33.47%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -6,7 +6,7 @@ class Solution:
             return result
         
         start = nums[0]
-        
+    
         for i in range(1, len(nums)):
             if nums[i] - nums[i - 1] != 1:
                 end = nums[i - 1]
