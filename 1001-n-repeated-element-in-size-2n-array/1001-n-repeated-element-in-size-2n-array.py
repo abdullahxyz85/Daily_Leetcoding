862b89d9fa1306c73f61c9e6445d0e049f9bb740
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-10-06 19:09:33 +0500

    Time: 8 ms (38.91%), Space: 18.9 MB (10.12%) - LeetHub

--- 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
-        freq = {}   # {1: 1, 2: 3, 3: 2}
+        freq = {}   # {1: 1, 2: 1, 3: 2}
 
         for num in nums:
             if num in freq:     
