a4f4fb89d152b73c5b70d43da4e471a500f13ed7
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-10-10 21:39:40 +0500

    Time: 0 ms (100%), Space: 17.8 MB (58.55%) - LeetHub

--- 1769-get-maximum-in-generated-array/1769-get-maximum-in-generated-array.py
+++ 1769-get-maximum-in-generated-array/1769-get-maximum-in-generated-array.py
@@ -8,7 +8,7 @@ class Solution:
         nums[0] = 0
         nums[1] = 1
         
-        for i in range(1, n // 2 + 1):  
+        for i in range(1, n + 1):  
             if 2 * i <= n:
                 nums[2 * i] = nums[i]
             if 2 * i + 1 <= n:
