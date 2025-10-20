9c54e57db10f49adddb64ef935105677d2c305ba
Abdullah Jameel <abdullahxyz85@gmail.com>
2025-10-20 18:46:08 +0500

    Time: 3 ms (9.6%), Space: 17.9 MB (13.04%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.py
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.py
@@ -1,12 +1,8 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
         if s.count('A') >= 2:
-            return False  # not eligible
-
-        # Condition 2: check if student was late 3 or more consecutive days
+            return False  
         if 'LLL' in s:
-            return False  # not eligible
-
-        # If both conditions are fine, eligible
+            return False  
         return True
 
