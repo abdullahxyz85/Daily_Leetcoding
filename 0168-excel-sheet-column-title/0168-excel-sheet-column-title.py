dbd4fadd37bd45080d33f23638b78f9a311b3c15
Abdullah Jameel <abdullahxyz85@gmail.com>
2026-04-05 10:17:01 +0500

    Time: 0 ms (100%), Space: 12.4 MB (64.7%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        """
+        :type columnNumber: int
+        :rtype: str
+        """
+        result = ""
+        
+        while columnNumber > 0:
+            columnNumber -= 1   
+            
+            remainder = columnNumber % 26
+            char = chr(65 + remainder)  
+            
+            result = char + result      
+            
+            columnNumber //= 26          
+        
+        return result
