cb0066539036873b482cbaaeca24bfd0e4fbc89f
Abdullah Jameel <abdullahxyz85@gmail.com>
2026-08-21 14:43:07 +0500

    Time: 0 ms (100%), Space: 12.4 MB (87.11%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -6,7 +6,6 @@ class Solution(object):
         :rtype: float
         """
 
-        # Make sure nums1 is the smaller array
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
 
@@ -18,13 +17,10 @@ class Solution(object):
 
         while left <= right:
 
-            # Partition nums1
             partition1 = (left + right) // 2
 
-            # Partition nums2
             partition2 = (m + n + 1) // 2 - partition1
 
-            # Values on the left side
             if partition1 == 0:
                 left1 = float('-inf')
             else:
@@ -35,7 +31,6 @@ class Solution(object):
             else:
                 left2 = nums2[partition2 - 1]
 
-            # Values on the right side
             if partition1 == m:
                 right1 = float('inf')
             else:
@@ -46,21 +41,16 @@ class Solution(object):
             else:
                 right2 = nums2[partition2]
 
-            # Correct partition
             if left1 <= right2 and left2 <= right1:
 
-                # Odd total length
                 if (m + n) % 2 == 1:
                     return max(left1, left2)
 
-                # Even total length
                 else:
                     return (max(left1, left2) + min(right1, right2)) / 2.0
 
-            # We need to move partition1 to the left
             elif left1 > right2:
                 right = partition1 - 1
 
-            # We need to move partition1 to the right
             else:
                 left = partition1 + 1
